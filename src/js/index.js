037d59e1deb9466e0960e9d40177f0709e436596
qjkiddy83 <qjkiddy-83@163.com>
2017-10-22 21:01:33 +0800

    skp

--- src/js/index.js
+++ src/js/index.js
@@ -1,24 +1,51 @@
 let $ = require('./zepto.min');
-$('.page,.wrapper').on('swipeUp', function() {
+$('.page,.wrapper,.end').on('swipeUp', function() {
+	let next = $(this).next('.page,.wrapper,.end');
+	if(next.hasClass('page')){
+		$('header').show();
+	}else{
+		$('header').hide();
+		if(next.hasClass('end')){
+			next.find('.logo img').attr('src','http://skptrends.koikreative.com/image/p0_title.gif?'+new Date().getTime())
+		}
+		// http://skptrends.koikreative.com/image/p0_title.gif?1508674406362
+	}
 	let cur_step = $(this).data('step') || 1,
 		max_step = $(this).data('maxstep');
 
 	if (cur_step < max_step) {
-		$(this).removeClass('init step' + cur_step).data('step', ++cur_step).addClass('step' + cur_step)
+		$(this).removeClass('down init step' + cur_step).data('step', ++cur_step).addClass('up step' + cur_step)
 	} else {
-		if (!$(this).next('.page,.wrapper').length) {
+		if (!$(this).next('.page,.wrapper,.end').length) {
 			return false;
 		}
-		$(this).data('step', 1).removeClass("current init").removeClass('step' + cur_step).next('.page,.wrapper').data('step', 1).addClass('current init step1')
+		$(this).data('step', 1).removeClass("current up down init").removeClass('step' + cur_step).next('.page,.wrapper,.end').data('step', 1).addClass('current up init step1')
 	}
 }).on('swipeDown', function() {
+	if($(this).prev().hasClass('page')){
+		$('header').show();
+	}else{
+		$('header').hide();
+	}
 	let cur_step = $(this).data('step');
 	if (cur_step > 1) {
-		$(this).removeClass('step' + cur_step).data('step', --cur_step).addClass('step' + cur_step)
+		$(this).removeClass('up init step' + cur_step).data('step', --cur_step).addClass('down step' + cur_step)
 	} else {
-		if (!$(this).prev('.page,.wrapper').length) {
+		if (!$(this).prev('.page,.wrapper,.end').length) {
 			return false;
 		}
-		$(this).data('step', 1).removeClass("current init").removeClass('step' + cur_step).prev('.page,.wrapper').data('step', 1).addClass('current init step1')
+		$(this).data('step', 1).removeClass("current up down init").removeClass('step' + cur_step).prev('.page,.wrapper,.end').data('step', 1).addClass('current down init step1')
 	}
 })
+
+$('#btn-share').tap(function(){
+	$('.sharelayer').show();
+})
+$('.sharelayer').tap(function(){
+	$(this).hide();
+})
+$('#btn-replay').tap(function(){
+	$('.page,.wrapper,.end').removeClass('current up down init').data('step',1);
+	$('.p1').addClass('current').find('.logo img').attr('src','http://skptrends.koikreative.com/image/p0_title.gif?'+new Date().getTime())
+})
+$('.p1').find('.logo img').attr('src','http://skptrends.koikreative.com/image/p0_title.gif?'+new Date().getTime())
